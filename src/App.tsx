93114f017ce343be2846d9a0ab67237ec6a149f5
WandersonWebDesing <seuemail@gmail.com>
2026-04-06 09:17:22 -0300

    sua alteração

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 // src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Páginas
+import Home from './pages/home';
+
 // Componentes
 import Header from './components/navbar';
 import Footer from './components/footer';
@@ -12,18 +15,18 @@ function App() {
 
       <main style={{ marginTop: '65px', minHeight: '80vh' }}>
         <Routes>
-          {/* Rota principal */}
-          <Route path="/" element={<div style={{ padding: '20px' }}>Página inicial</div>} />
+          {/* ✅ Rota principal */}
+          <Route path="/" element={<Home />} />
 
-          {/* ✅ Rota 404 (Página não encontrada) */}
-          <Route 
-            path="*" 
+          {/* ✅ Rota 404 */}
+          <Route
+            path="*"
             element={
               <div style={{ padding: '20px', textAlign: 'center' }}>
                 <h1>404</h1>
                 <p>Página não encontrada</p>
               </div>
-            } 
+            }
           />
         </Routes>
       </main>
